4306a9682403bb60ce06cef7300419e79bbda45a
XPS17 <github_ww@wegerson.com>
2022-03-09 05:45:00 -0700

    Middling component work

---
+++ JSON Enumerate/Implementation/BlazoriseComponentProperty.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JSON_Enumerate.Operation;
+
+namespace JSON_Enumerate.Implementation
+{
+    public class BlazoriseComponentProperty : BareJsonProperty
+    {
+
+        public override string ToString() => Name;
+        public  string ToDataGridColumn(string dto)
+        {
+            //            return Name;
+            var sb = new StringBuilder();
+
+            // $"\t<DataGridColumn TItem=\x22{dto}\x22 Field=\x22nameof({dto}.{prp})\x22 Caption=\x22{prp}\x22 />"));
+
+     //       sb.Append($"\t<DataGrid");
+
+            switch (JsonType)
+            {
+                case JsonPropType.Undefined:
+                case JsonPropType.StrType:
+                    if (IsDateTime)
+                        sb.Append(GenerateDate());
+                    break;
+
+                case JsonPropType.NumberType:
+                    sb.Append("Numeric");
+                    break;
+
+                case JsonPropType.BoolType:
+                    sb.Append("Check");
+                    break;
+
+                case JsonPropType.UserType:
+                    sb.Append($"Select");
+                    break;
+
+            }
+
+            sb.Append($"Column TItem=\x22{dto}\x22 Editable=\x22true\x22 Caption=\x22{Name}\x22 Field=\x22nameof({dto}.{Name})\x22  />");
+
+            return sb.ToString();
+        }
+
+        public string GenerateDate()
+            => @$"
+<Field ColumnSize=""ColumnSize.Is2.OnDesktop"">
+    <FieldLabel>{Name}</FieldLabel>
+    <DatePicker @bind-Date=""@VM.{Name}"" TValue=""DateTime?"" />
+</Field>
+"
+;
+    }
+}
---
+++ JSON Enumerate/Implementation/BlazorizeComponent.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using JSON_Enumerate.Operation;
+
+namespace JSON_Enumerate.Implementation;
+
+public class BlazorizeComponent : JsonOperationBase
+{
+    public int IdentityStart { get; set;  }
+
+    public BlazorizeComponent() {}
+
+    public BlazorizeComponent(IJsonSettings settings, ICommand overrideProperty=null) : base(settings, overrideProperty) { }
+
+
+    public override string ToString()
+    {
+        var sb=new StringBuilder();
+
+        if (IsNameUndefined)
+            Name=Settings.Name;
+
+        var dto=$"{Name}DTO";
+
+        //sb.AppendLine($"<DataGrid TItem=\x22{dto}\x22 Field=\x22@nameof(ListOf{Name}s)\x22>");
+        sb.AppendLine(GridNodeStart());
+        
+        var props=Properties.Cast<BlazoriseComponentProperty>();
+
+        if (SettingsSingleton.Settings.IsSortProperties)
+            props=props.OrderBy(nm => nm.Name).ThenBy(prp => prp.IsId());
+
+        sb.AppendJoin($",{Environment.NewLine}", props.Select(prp => prp.ToDataGridColumn(dto)));
+
+        //sb.AppendLine($"{Environment.NewLine}</DataGrid>");
+  //      sb.AppendLine(GridNodeEnd());
+
+  //      sb.AppendLine(Code());
+
+        return sb.ToString();
+    }
+
+    public string GridNodeStart() => @$"
+
+<Card Margin=""Margin.Is4.OnY"">
+<CardHeader>
+    <CardTitle>{Name}</CardTitle>
+</CardHeader>
+    <CardBody>
+
+    </CardBody>
+</Card>
+";
+
+   public string GridNodeEnd() => @$"
+    </DataGridColumns>
+    <ButtonRowTemplate>
+         <Button Color=""Color.Success"" Clicked=""@context.NewCommand.Clicked"">@context.NewCommand.LocalizationString</Button>
+         <Button Color=""Color.Primary"" Clicked=""@context.EditCommand.Clicked"" Disabled=""@(selectedAccount is null)"">@context.EditCommand.LocalizationString</Button>
+         <Button Color=""Color.Danger"" Clicked=""@context.DeleteCommand.Clicked"" Disabled=""@(selectedAccount is null)"">@context.DeleteCommand.LocalizationString</Button>
+         <Button Color=""Color.Warning"" Clicked=""@context.ClearFilterCommand.Clicked"">@context.ClearFilterCommand.LocalizationString</Button>
+    </ButtonRowTemplate>
+</DataGrid>
+";
+
+    public string Code() => @$"
+@code {{
+    private List<{Name}DTO>? {Name}sList {{ get; set; }}
+    private {Name}DTO selected{Name};
+
+    protected override async Task OnInitializedAsync()
+    {{
+        {Name}sList = (await {Name}Service.GetAll{Name}s()).ToList() ?? null;
+
+        await base.OnInitializedAsync();
+    }}
+
+    async Task OnRowInserted(SavedRowItem<{Name}DTO, Dictionary<string, object>> e){{
+        Console.WriteLine($""{{e.Item.Name}} Created"");
+    }}
+    
+    async Task OnRowUpdated(SavedRowItem<{Name}DTO, Dictionary<string, object>> e){{
+        Console.WriteLine($""{{e.Item.Name}} Saved"");
+    }}
+
+}}";
+}
